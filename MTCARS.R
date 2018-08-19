2eac685317994d54c2d1556f00113659ca4a5328
pgayatri7 <gayatri.p18038@iimtrichy.ac.in>
2018-08-19 19:52:55 +0530

    x and y vector values

--- MTCARS.R
+++ MTCARS.R
@@ -3,3 +3,11 @@ git config --global user.email "gayatri.p18038@iimtrichy.ac.in"
 git config --global user.name "pgayatri7"
 women
 women
+plot(women)
+?women
+x=1:100
+?women
+x=1:100
+y
+x
+y=seq(1,100,by=2)
